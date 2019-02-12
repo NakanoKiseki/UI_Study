55b97bcbd57dedd6824d40136fa9c72d2687e57e
k.nakano <k.nakano@symmetryvr.com>
2019-02-12 13:24:38 +0900

    Add twitter ui

---
+++ Assets/Sample/Data/Scripts/SampleTweetMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Twitter.UI;
+
+public class SampleTweetMain : MonoBehaviour
+{
+    [Serializable]
+    public struct TweetData
+    {
+        public string UserName;
+        public string Message;
+        public Sprite UserIcon;
+    }
+
+    [SerializeField]
+    private GameObject _tweetPrefab;
+
+    public TweetData[] TweetsData;
+    
+    private List<Tweet> _tweetList = new List<Tweet>();
+
+    void Start()
+    {
+        GenerateTweets();
+    }
+
+    #region PRIVATE METHODS
+
+    private void GenerateTweets()
+    {
+        foreach (var t in TweetsData)
+        {
+            var tweetUI = Instantiate<GameObject>(_tweetPrefab, this.transform);
+            var tweet = tweetUI.GetComponent<Tweet>();
+            _tweetList.Add(tweet);
+
+            tweet.SetUserName(t.UserName);
+            tweet.SetMessage(t.Message);
+            tweet.SetUserIcon(t.UserIcon);
+            tweet.SetRetweetButtonOnClick(() => { Debug.Log("Retweeted!"); });
+            tweet.SetLikesButtonOnClick(() => { Debug.Log("Liked!"); });
+
+            var tweetPos = new Vector3(0f, 0f, 0f);
+            tweetPos.y = _tweetList.Count * 1.6f;
+            tweetUI.transform.position = tweetPos;
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Twitter/UI/Tweet/Tweet.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Twitter.UI
+{
+    public class Tweet : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _userNameText;
+        [SerializeField]
+        private Text _messageText;
+        [SerializeField]
+        private Button _retweetButton;
+        [SerializeField]
+        private Button _likesButton;
+        [SerializeField]
+        private Image _userIconImage;
+
+        public Action OnRetweetButtonClickAction { get; private set; }
+        public Action OnLikesButtonClickAction { get; private set; }
+
+        public void SetUserName(string userNameText)
+        {
+            this._userNameText.text = userNameText;
+        }
+
+        public void SetMessage(string messageText)
+        {
+            _messageText.text = messageText;
+        }
+
+        public void SetRetweetButtonOnClick(Action onRetweet)
+        {
+            OnRetweetButtonClickAction = onRetweet;
+        }
+
+        public void SetLikesButtonOnClick(Action onLike)
+        {
+            OnLikesButtonClickAction = onLike;
+        }
+
+        public void SetUserIcon(Sprite userIcon)
+        {
+            _userIconImage.sprite = userIcon;
+        }
+
+        #region PRIVATE METHODS
+
+        private void Awake()
+        {
+            OnRetweetButtonClickAction = () => { };
+            OnLikesButtonClickAction = () => { };
+        }
+
+        private void Start()
+        {
+            _retweetButton.onClick.AddListener(OnRetweetClick);
+            _likesButton.onClick.AddListener(OnLikesClick);
+        }
+
+        private void OnRetweetClick()
+        {
+            Debug.Log("OnRetweetClick");
+            OnRetweetButtonClickAction();
+        }
+
+        private void OnLikesClick()
+        {
+            OnLikesButtonClickAction();
+        }
+
+        #endregion
+    }
+}
